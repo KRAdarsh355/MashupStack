b496961409b7753c9bf398eaddf4e329f07bdefc
KR Adarsh <adrsh7878@gmail.com>
2026-09-17 11:58:29 +0530

    sqlDay2

---
+++ SQL/classwork2.sql
@@ -0,0 +1,5 @@
+INSERT INTO `students`(`id`, `name`, `age`, `department`, `grade`) VALUES (1,'Dave',18,'CSE',86),(2,'Liya',17,'CSE',66),(3,'Jon',19,'ECE',74),(4,'Mikey',18,'Mech',81),(5,'Patrick',18,'IT',69);
+SELECT * FROM `students` WHERE age>20
+SELECT * FROM `students` WHERE department='CSE' OR department="Physics"
+SELECT name FROM `students` WHERE grade=90;
+SELECT name FROM `students` WHERE grade BETWEEN 70 AND 90;
---
+++ SQL/homework2.sql
@@ -0,0 +1,5 @@
+INSERT INTO `books`(`id`, `title`, `author`, `price`, `genre`) VALUES (1,'Don Quixote','Miguel de Cervantes',500,'Classic'),(2,'To Kill a Mocking Bird','Harper Lee',650,'Gothic'),(3,'The DaVinci Code','Dan Brown',710,'Mystery Thriller'),(4,'Atomic Habits','James Clear',790,'Self-improvement Guide'),(5,'The Alchemist','Paulo Coelho',900,'Fable')
+SELECT `title` FROM `books` WHERE price>400;
+SELECT  `title `FROM `books` WHERE genre IN('History','Science','Fiction');
+SELECT `title` FROM `books` WHERE title='The Great Gatsby';
+SELECT `title` FROM `books` WHERE author!='Dan Brown';
